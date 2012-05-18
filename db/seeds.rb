198894702a8448501c408d78c40ea1645ebc385f
BoYu <boyu2011@gmail.com>
2012-05-18 14:10:01 -0400

    add product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create(:title => 'CoffeeScript',
+               :description => %{<p>CoffeeScript is JavaScript done right. It provides all of JavaScript's functionality wrapped in a cleaner, more succinct syntax.</p>},
+               :image_url => 'cs.jpg',
+               :price => 36.00)
+#...
+Product.create(:title => 'Programming Ruby 1.9',
+               :description => %{Ruby is the fastest growing and most exciting dynamic language
+                                out there.},
+               :image_url => 'ruby.jpg',
+               :price => 49.95)
+#...
+Product.create(:title => 'Rails Test Prescriptions',
+               :description => %{This is a comprehensive guide to testing Rails applications.},
+               :image_url => 'rtp.jpg',
+               :price => 34.95)
