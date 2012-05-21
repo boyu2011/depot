ba760845df9867c950e6b4c59b4ddea36b3efccc
BoYu <boyu2011@gmail.com>
2012-05-21 00:37:34 -0400

    create a smarter cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,12 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   attr_accessible :product
+  attr_accessible :quantity
   
   belongs_to :product
   belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
 end
