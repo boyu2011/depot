213f8b595951512f3f55f47f9f78145e9844d963
BoYu <boyu2011@gmail.com>
2012-05-27 19:44:59 -0400

    implement checkout

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,9 +2,11 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   attr_accessible :product
   attr_accessible :quantity
+  attr_accessible :cart
   
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
   
   def total_price
     product.price * quantity
