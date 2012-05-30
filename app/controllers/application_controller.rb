0573a11d0d8c83fedf7f2b500887b3d927a59104
BoYu <boyu2011@gmail.com>
2012-05-30 12:51:08 -0400

    add administration system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authorize
   
   private 
   
@@ -10,4 +12,12 @@ class ApplicationController < ActionController::Base
         session[:cart_id] = cart.id
         cart
     end
+
+  protected
+
+  	def authorize
+  		unless User.find_by_id(session[:user_id])
+  			redirect_to login_url, :notice => "Please log in"
+  		end
+  	end
 end
